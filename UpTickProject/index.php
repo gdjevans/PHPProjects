047a13c5ca808e16ced3ca354b0c7e3cd6a40bbd
DESKTOP-6FBGBC6\GDJEva <gdjevans@hotmail.com>
2022-11-22 21:08:53 +0000

    Setup Intro Page for searching

---
+++ UpTickProject/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome to UpTick</title>
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css"/>
+</head>
+<body>
+    <div class="wrapper indexPage">
+        <div class="mainSection">
+            <div class="logoContainer">
+                <img src="assets/imgs/logo.jpg"/>
+            </div>
+
+            <div class="searchContainer">
+                <form action="search.php" method="GET">
+
+                    <input class="searchBox" type="text" name="term" placeholder="Enter a search term..."/>
+                    <input class="searchButton" type="submit" value="Search"/>
+
+                </form>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
