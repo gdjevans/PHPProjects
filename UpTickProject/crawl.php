bf3a956ef13605268935d5fc52ccacdac2fa4365
DESKTOP-6FBGBC6\GDJEva <gdjevans@hotmail.com>
2022-12-04 05:08:09 +0000

    Create Parser for scraping html links on specific page.

--- UpTickProject/crawl.php
+++ UpTickProject/crawl.php
@@ -4,6 +4,14 @@ include("classes/DomDocumentParser.php");
 
 function followLinks($url) {
     $parser = new DomDocumentParser($url);
+
+    $linkList = $parser->getLinks();
+
+    foreach($linkList as $link) {
+        $href = $link->getAttribute("href");
+        echo $href . "<br />";
+    }
+
 }
 
 $startUrl = "http://www.gdjevans.com";
