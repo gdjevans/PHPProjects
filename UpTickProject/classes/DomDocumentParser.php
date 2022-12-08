f47e888e4c98700323bda6066f5eff8e66d73f68
DESKTOP-6FBGBC6\GDJEva <gdjevans@hotmail.com>
2022-12-08 00:51:55 +0000

    UpTick web crawling links

--- UpTickProject/classes/DomDocumentParser.php
+++ UpTickProject/classes/DomDocumentParser.php
@@ -9,12 +9,19 @@ class DomDocumentParser {
         $context = stream_context_create($options);
 
         $this->doc = new DomDocument();
-        @$this->doc->loadHTML(file_get_contents($url, false, $context));
-
+        @$this -> doc -> loadHTML( '<?xml encoding="utf-8"?>' . file_get_contents($url, false, $context));
     }
 
     public function getLinks() {
         return $this->doc->getElementsByTagName("a");
+    }
+
+    public function getTitleTags() {
+        return $this->doc->getElementsByTagName("title");
+    }
+    
+    public function getMetaTags() {
+        return $this->doc->getElementsByTagName("meta");
     }
 }
 
--- UpTickProject/index.php
+++ UpTickProject/index.php
@@ -2,6 +2,9 @@
 <html lang="en">
     <head>
         <meta charset="UTF-8">
+        <meta name="description" content="Search the web for sites and images">
+        <meta name="keywords" content="Search engine, uptick, up, tick, websites">
+        <meta name="author" content="GDJEvans">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Welcome to UpTick</title>
         <link rel="stylesheet" type="text/css" href="assets/css/style.css"/>
