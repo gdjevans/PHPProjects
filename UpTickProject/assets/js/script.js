e4265fa02da6dd2be2bb1ce50d763722f27ab0b1
DESKTOP-6FBGBC6\GDJEva <gdjevans@hotmail.com>
2022-12-26 15:52:43 +0000

    Images section. Display imgs and use third party masonry css.

--- UpTickProject/assets/js/script.js
+++ UpTickProject/assets/js/script.js
@@ -1,3 +1,5 @@
+var timer;
+
 $(document).ready(function() {
 	
     $(".result").on("click", function() {
@@ -14,7 +16,43 @@ $(document).ready(function() {
         return false;
     });
 
+    var grid = $(".imageResults");
+
+    grid.on("layoutComplete", function() {
+        $(".gridItem img").css("visibility", "visible");
+    });
+
+    grid.masonry({
+        itemSelector: ".gridItem",
+        columnWidth: 200,
+        gutter: 5,
+        isInitLayout: false
+    });
+
 });
+
+function loadImage(src, className) {
+    
+    var image = $("<img>");
+
+    image.on("load", function() {
+        $("." + className + " a").append(image);
+
+        clearTimeout(timer);
+
+        timer = setTimeout(function() {
+            $(".imageResults").masonry();
+        }, 500);
+
+        
+    });
+
+    image.on("error", function() {
+        
+    });
+
+    image.attr("src", src);
+}
 
 function increaseLinkClicks(linkId, url) {
     $.post("ajax/updateLinkCount.php", {linkId: linkId})
